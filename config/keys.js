f67c3464a43d0700db3f7a16f1d5f74cc506afd9
anuradha-96 <anuradhapcs95@gmail.com>
2020-11-24 15:22:13 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '652394403601-otv7agg1cogl0fn0m84s2o080ppv235s.apps.googleusercontent.com',
+	googleClientSecret: 'pv-zX412SgfmJie7iWjlS6m4',
+	mongoURI: 'mongodb+srv://p00gz:PF9Ms6HayftCmiD1@emaily.2s4ml.mongodb.net/test?retryWrites=true&w=majority'
+};
+
