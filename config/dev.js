0c1ab3f17374cd5cfbac126ca66be4913a98e3df
anuradha-96 <anuradhapcs95@gmail.com>
2020-12-15 15:22:18 +0530

    updating

--- config/dev.js
+++ config/dev.js
@@ -5,7 +5,7 @@ module.exports = {
 	googleClientSecret: 'HPHfSYfrBmPXF9r_oNAPri2y',
 	mongoURI: 'mongodb+srv://p00gz:PF9Ms6HayftCmiD1@emaily.2s4ml.mongodb.net/test?retryWrites=true&w=majority',
 	cookieKey: 'dghfhgvhvhvhvhgvhvhvhvhvjvjvytsuyefsdfbhbdjhfdh',
-	StripePublishableKey: 'pk_test_51HuYGgBlSIm87cz0Xws3h03Pr2sqdmGak0oJmE8FJQpAt8fnfkUmM15pO08eQkwyvGNHXH4myDIJeprSo8jWgbUV00sNSXS1tY',
-	StripeSecretKey: 'sk_test_51HuYGgBlSIm87cz0X9GiPf1h2PWGFZ7k64cVLtMSR8jLa6WmL5TvdYT2ADHp6sPMAZ8nQziCbq3cYBHgOJreMhtr00dcpEHGZ9'
-};
+	stripePublishableKey: 'redacted',
+  stripeSecretKey: 'redacted',
 
+};
