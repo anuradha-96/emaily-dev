30347ddc1f4c0e0151d3e33a963aef099a6016b4
anuradha-96 <anuradhapcs95@gmail.com>
2020-12-22 14:44:29 +0530

    heroku

--- index.js
+++ index.js
@@ -52,8 +52,12 @@ if (process.env.NODE_ENV === ' production ') {
 
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
 
 
 
