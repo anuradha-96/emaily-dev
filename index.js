a8bd6f9ce3ca05ed4c789e3666b1069def0d832f
anuradha-96 <anuradhapcs95@gmail.com>
2020-12-22 14:17:28 +0530

    client side app

--- index.js
+++ index.js
@@ -29,6 +29,29 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 
+
+if (process.env.NODE_ENV === ' production ') {
+	
+	// express will serve up production assets
+	// like our main.js or main .css file
+	
+	app.use(express.static('client/build'));
+	
+	
+	// express will serve up index.html 
+	// if it doesnot recognize route events
+	
+	const path = require('path');
+	app.get('*', (req, res)  => {
+		
+		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+		
+	});
+	
+}
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
