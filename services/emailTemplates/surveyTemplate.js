23ea23bfecdbb1cad5ffab41ea41d05c0cdbbd4d
anuradha-96 <anuradhapcs95@gmail.com>
2021-01-02 17:50:44 +0530

    make it better

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,6 +1,28 @@
 module.exports = (survey) => {
 	
-	return '<div>' + survey.body + '</div>'
+return 
+
+<html>
+
+<body>
+<div style="text-align: center;">
+<h3>I'd like your input!</h3>
+<p>please answer folllowing questions:</p>
+<p>${survey.body}</p>
+<div>
+ <a href="https://localhost:3000">yes</a>
+</div>
+<div>
+<a href="http://localhost:3000">no</a>
+
+</div>
+</div>
+</body>
+</html>
+
+
+
+	;
 	
 	
 };
